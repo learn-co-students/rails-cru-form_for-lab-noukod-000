7e27e2936e48982bf343facb2faca1890a55eae5
Gilbert Torchon <gilbert@noukod.com>
2019-01-22 14:48:42 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,38 @@
+class SongsController < ApplicationController
+
+      def index
+        @songs=Song.all
+      end
+
+      def new
+        @song=Song.new
+        @artists=Artist.all
+        @genres=Genre.all
+      end
+
+      def show
+        @song=Song.find(params[:id])
+      end
+
+      def edit
+        @song=Song.find(params[:id])
+      end
+
+      def create
+        @song=Song.create(songs_params(:name))
+        redirect_to song_path(@song)
+      end
+
+      def update
+        @song=Song.find(params[:id])
+        @song.update(songs_params(:name))
+        redirect_to song_path(@song)
+      end
+
+      def songs_params(*args)
+        params.require(:song).permit(*args)
+      end
+
+
+
+end
