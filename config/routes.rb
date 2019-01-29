ea6574302deee55351b02cd98026727a232b1a91
clifford <clifford@noukod.com>
2019-01-29 21:02:54 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  # resources :genres, only: [:index, :show, :new, :create, :edit, :update]
+  # resources :artists, only: [:index, :show, :new, :create, :edit, :update]
+  # resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+
   get 'genres/new'
 
   get 'genres/create'
