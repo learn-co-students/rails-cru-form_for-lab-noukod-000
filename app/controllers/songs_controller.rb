4c83b0e405900d1aa15419aa90896beb60e63499
Merline Belizaire <merline@noukod.com>
2019-01-29 16:43:20 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,36 +1,36 @@
 
 class SongsController < ApplicationController
+	def index
+		@songs = Song.all
+	end
 
-      def index
-        @songs=Song.all
-      end
+	def show
+		@song = Song.find(params[:id])
+	end
 
-      def new
-        @song=Song.new
-        @artists=Artist.all
-        @genres=Genre.all
-      end
+	def new
+		@song = Song.new
+	end
 
-      def show
-        @song=Song.find(params[:id])
-      end
+	def create
+	  @song = Song.new(song_params)
+	  @song.save
+	  redirect_to song_path(@song)
+	end
 
-      def edit
-        @song=Song.find(params[:id])
-      end
+	def edit
+		@song = Song.find(params[:id])
+	end
 
-      def create
-        @song=Song.create(songs_params(:name))
-        redirect_to song_path(@song)
-      end
+	def update
+	  @song = Song.find(params[:id])
+	  @song.update(song_params)
+	  redirect_to song_path(@song)
+	end
 
-      def update
-        @song=Song.find(params[:id])
-        @song.update(songs_params(:name))
-        redirect_to song_path(@song)
-      end
+	private
 
-      def songs_params(*args)
-        params.require(:song).permit(*args)
-      end
+		def song_params
+			params.require(:song).permit(:name, :artist_id, :genre_id)
+		end
 end
