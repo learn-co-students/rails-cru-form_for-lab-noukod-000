bf9278fb78eace4dca6c63072b970346ed83ddd7
Merline Belizaire <merline@noukod.com>
2019-01-29 16:27:39 +0000

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,33 @@
+
+class ArtistsController < ApplicationController
+  def index
+    @artist=Artist.all
+  end
+
+  def new
+    @artist=Artist.new
+  end
+
+  def show
+    @artist=Artist.find(params[:id])
+  end
+
+  def edit
+    @artist=Artist.find(params[:id])
+  end
+
+  def create
+    @artist=Artist.create(artists_params(:name,:bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def update
+    @artist=Artist.find(params[:id])
+    @artist.update(artists_params(:name,:bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def artists_params(*args)
+    params.require(:artist).permit(*args)
+  end
+end
