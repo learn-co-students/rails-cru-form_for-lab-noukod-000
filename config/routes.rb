9ff9ccc07c8733e2129efaaecf8984d5cdc2f83a
Jerrycharles44 <jerrycharles586@gmail.com>
2019-08-28 12:06:52 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    resources :artists, only: [:index, :show, :create, :update, :new, :edit, :destroy]
+    resources :songs, only: [:index, :show, :create, :update, :new, :edit, :destroy]
+    resources :genres, only: [:index, :show, :create, :update, :new, :edit, :destroy]
 
   # Example resource route with options:
   #   resources :products do
